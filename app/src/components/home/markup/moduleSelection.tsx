ebcd41d7ecc505ab4b01e52e19b4bbefdd377a59
ssskram <paul.marks@pittsburghpa.gov>
2019-02-28 17:15:01 -0500

    home styles

--- app/src/components/home/markup/moduleSelection.tsx
+++ app/src/components/home/markup/moduleSelection.tsx
@@ -4,6 +4,15 @@ type props = {
     setState: (stateObj: object) => void
 }
 
+const server = require('../../../images/server.png')
+const api = require('../../../images/api.png')
+const computer = require('../../../images/computer.png')
+const micro = require('../../../images/network.png')
+
+const imgStyle = {
+    height: '50px'
+}
+
 export default class ModuleSelection extends React.Component<props, {}> {
 
     render() {
@@ -12,22 +21,31 @@ export default class ModuleSelection extends React.Component<props, {}> {
                 <button
                     onClick={() => this.props.setState({ currentModule: 'appservices' })}
                     className='btn btn-secondary'>
-                    Application services
+                    <img src={server as string} style={imgStyle}></img>
+                    <h4 className='oswald-header'>Application services</h4>
+                    <div>The computing resources</div>
                 </button>
                 <button
                     onClick={() => this.props.setState({ currentModule: 'apis' })}
                     className='btn btn-secondary'>
-                    APIs
+                    <img src={api as string} style={imgStyle}></img>
+                    <h4 className='oswald-header'>Interfaces</h4>
+                    <div>The middlemen.  The body guards</div>
                 </button>
                 <button
                     onClick={() => this.props.setState({ currentModule: 'clientapps' })}
                     className='btn btn-secondary'>
-                    Client applications
+                    <img src={computer as string} style={imgStyle}></img>
+                    <h4 className='oswald-header'>Client apps</h4>
+                    <div>The UI and business logic</div>
                 </button>
                 <button
                     onClick={() => this.props.setState({ currentModule: 'serverless' })}
                     className='btn btn-secondary'>
-                    Serverless functions
+                    <img src={micro as string} style={imgStyle}></img>
+                    <h4 className='oswald-header'>Serverless</h4>
+                    <div><s>Everything else</s></div>
+                    <div className='script'>Microservices</div>
                 </button>
             </div>
         )
