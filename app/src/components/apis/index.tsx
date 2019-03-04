4d4cfc6b69165c82f9b61e29cbc180de135f1242
ssskram <ssskram@ssskram.local>
2019-03-04 13:26:34 -0500

    paging on all but services

--- app/src/components/apis/index.tsx
+++ app/src/components/apis/index.tsx
@@ -1,18 +1,51 @@
 import * as React from 'react'
 import * as types from '../../store/types'
 import ApplicationCard from '../shared/applicationCard'
+import Paging from '../utilities/paging'
 
 type props = {
     apiApps: types.application[]
 }
 
-export default class APIs extends React.Component<props, {}> {
+type state = {
+    currentPage: number
+    items: any
+}
+
+export default class APIs extends React.Component<props, state> {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentPage: 1,
+            items: undefined
+        }
+    }
 
     render() {
-        return (
-            <div>
-                {this.props.apiApps.map((api, index) => <ApplicationCard key={index} application={api} />)}
-            </div>
-        )
+
+        // Logic for paging
+        const indexOfLastItem = this.state.currentPage * 10
+        const indexOfFirstItem = indexOfLastItem - 10;
+        const currentItems = this.props.apiApps.slice(indexOfFirstItem, indexOfLastItem);
+        const renderItems = currentItems.map((item, index) => {
+            return <ApplicationCard key={index} application={item} />
+        })
+
+        // Logic for displaying page numbers
+        const pageNumbers: any[] = []
+        for (let i = 1; i <= Math.ceil(this.props.apiApps.length / 10); i++) {
+            pageNumbers.push(i);
+        }
+
+        return <div>
+            {renderItems}
+            <Paging
+                countItems={this.props.apiApps}
+                currentPage={this.state.currentPage}
+                totalPages={pageNumbers}
+                next={() => this.setState({ currentPage: this.state.currentPage + 1 })}
+                prev={() => this.setState({ currentPage: this.state.currentPage - 1 })}
+            />
+        </div>
     }
 }
