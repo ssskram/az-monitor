f936e7427f35ae6fdceedb83d60dbe7b9b74a315
ssskram <paul.marks@pittsburghpa.gov>
2019-04-26 11:27:45 -0400

    contrinutor group

--- app/src/functions/contributors.ts
+++ app/src/functions/contributors.ts
@@ -2,5 +2,6 @@ export const contributors = [
     "paul.marks@pittsburghpa.gov",
     "geoffrey.arnold@pittsburghpa.gov",
     "nicholas.hall@pittsburghpa.gov",
-    "james.otoole@pittsburghpa.gov"
+    "james.otoole@pittsburghpa.gov",
+    "laura.meixell@pittsburghpa.gov"
 ]
